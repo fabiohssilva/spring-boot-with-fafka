827e5362adc8ac2d34fcfd66208ab704b356af68
INDRA\fhenriqued <fhenriqued@indracompany.com>
2020-10-30 13:47:30 -0300

    first commit

---
+++ src/main/java/com/demo/kafka/springbootwithfafka/SpringBootWithFafkaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.springbootwithfafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithFafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithFafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/kafka/springbootwithfafka/SpringBootWithFafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.kafka.springbootwithfafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithFafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
